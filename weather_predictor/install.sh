1e44159144660570767918360b80a482c1ce1ad0
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2016-12-21 15:57:45 +0100

    GraalVM copied to the working directory

--- weather_predictor/install.sh
+++ weather_predictor/install.sh
@@ -3,6 +3,7 @@
 CRAN_MIRROR=http://cran.us.r-project.org
 
 if [ ! -d openweather ]; then
+	echo "Installing openweather gem..."
 	# Uncomment after the proxy settings fix is merged into lucasocon repo
 	# git clone https://github.com/lucasocon/openweather.git
 	git clone https://github.com/zslajchrt/openweather.git
@@ -11,5 +12,16 @@ if [ ! -d openweather ]; then
 	cd ..
 fi
 
-$GRAALVM_DIR/bin/Rscript -e "install.packages('maps', repos='$CRAN_MIRROR')"
+# Install the maps package if not installed yet
+if [ ! -d $GRAALVM_DIR/language/R/library/maps ]; then
+	echo "Installing maps package..."
+
+	$GRAALVM_DIR/bin/Rscript -e "install.packages('maps', repos='$CRAN_MIRROR')"
+
+	FASTR_LOG=$GRAALVM_DIR/language/R/fastr_errors.log
+	if [ -f FASTR_LOG ]; then
+		echo "Dumping fastr_errors.log:"
+		cat $GRAALVM_DIR/language/R/fastr_errors.log
+	fi
+fi
 
