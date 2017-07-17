78c0ca0c59a94b71d420d44ee7b7c77aa5f9da00
Tomas Zezula <tomas.zezula@oracle.com>
2017-07-17 17:52:42 +0200

    Added a possibility to read default arguments from environment.

--- weather_predictor/run.sh
+++ weather_predictor/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$GRAALVM_DIR/bin/node --jvm --jvm.Xss2m --jvm.Dtruffle.js.NashornJavaInterop=true --jvm.classpath=./bin --ruby.load_paths=openweather/lib,. --polyglot weatherServer.js
+$GRAALVM_DIR/bin/node ${GRAALVM_ADDITIONAL_ARGS} --jvm --jvm.Xss2m --jvm.Dtruffle.js.NashornJavaInterop=true --jvm.classpath=./bin --ruby.load_paths=openweather/lib,. --polyglot weatherServer.js
