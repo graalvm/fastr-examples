ac286aefdda026a25cc3472cfcf42e5acd8f8ea6
Martin Balin <martin.balin@oracle.com>
2017-07-21 09:33:32 +0200

    Moving #!/bin/bash to 1st line of scripts

--- weather_predictor/run.sh
+++ weather_predictor/run.sh
@@ -1,3 +1,3 @@
-# Runs the demo without any tools enabled and printing all exceptions to terminal
 #!/bin/bash
+# Runs the demo without any tools enabled and printing all exceptions to terminal
 $GRAALVM_DIR/bin/node ${GRAALVM_ADDITIONAL_ARGS} --jvm --jvm.Xss2m --jvm.Dtruffle.js.NashornJavaInterop=true --jvm.classpath=./bin --ruby.load_paths=openweather/lib,. --polyglot  weatherServer.js
--- weather_predictor/tools.sh
+++ weather_predictor/tools.sh
@@ -1,4 +1,4 @@
-# Run demo with tools enabled and with exceptions being printed to command line
 #!/bin/bash
+# Run demo with tools enabled and with exceptions being printed to command line
 $GRAALVM_DIR/bin/node ${GRAALVM_ADDITIONAL_ARGS} --jvm --jvm.Xss2m --jvm.Dtruffle.js.NashornJavaInterop=true --jvm.classpath=./bin --ruby.load_paths=openweather/lib,. --polyglot --inspect --inspect.Suspend=false --agent weatherServer.js
 
